c7882c9ff8fc3e61014b3049061d11f0c2770386
Felipe Valeriano <101677047+FelipeValeriano21@users.noreply.github.com>
2023-01-20 17:22:09 -0300

    Add files via upload

---
+++ assets/js/controle.js
@@ -0,0 +1,270 @@
+
+
+function teclabranca1() {
+  const audio = document.getElementById('audiotb1');
+  audio.play();
+
+}
+
+function teclabranca2() {
+    const audio = document.getElementById('audiotb2');
+    audio.play();
+  }
+
+  
+  function teclabranca3() {
+    const audio = document.getElementById('audiotb3');
+    audio.play();
+  }
+
+  
+  function teclabranca4() {
+    const audio = document.getElementById('audiotb4');
+    audio.play();
+  }
+
+  
+  function teclabranca5() {
+    const audio = document.getElementById('audiotb5');
+    audio.play();
+  }
+
+  
+  function teclabranca6() {
+    const audio = document.getElementById('audiotb6');
+    audio.play();
+  }
+
+  
+  function teclabranca7() {
+    const audio = document.getElementById('audiotb7');
+    audio.play();
+  }
+
+  function teclabranca8() {
+    const audio = document.getElementById('audiotb8');
+    audio.play();
+  }
+  
+
+  function teclabranca9() {
+    const audio = document.getElementById('audiotb9');
+    audio.play();
+  }
+  
+
+  function teclabranca10() {
+    const audio = document.getElementById('audiotb10');
+    audio.play();
+  }
+  
+
+  function teclabranca11() {
+    const audio = document.getElementById('audiotb11');
+    audio.play();
+  }
+  
+
+  function teclabranca12() {
+    const audio = document.getElementById('audiotb12');
+    audio.play();
+  }
+  
+
+  function teclabranca13() {
+    const audio = document.getElementById('audiotb13');
+    audio.play();
+  }
+  
+
+  function teclabranca14() {
+    const audio = document.getElementById('audiotb14');
+    audio.play();
+  }
+  
+  function teclabranca15() {
+    const audio = document.getElementById('audiotb15');
+    audio.play();
+  }
+  
+
+  function teclabranca16() {
+    const audio = document.getElementById('audiotb15');
+    audio.play();
+  }
+
+  
+  function teclabranca17() {
+    const audio = document.getElementById('audiotb16');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca18() {
+    const audio = document.getElementById('audiotb17');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca18() {
+    const audio = document.getElementById('audiotb18');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca19() {
+    const audio = document.getElementById('audiotb19');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca20() {
+    const audio = document.getElementById('audiotb20');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca21() {
+    const audio = document.getElementById('audiotb21');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca22() {
+    const audio = document.getElementById('audiotb22');
+    audio.play();
+  }
+  
+
+  
+  function teclabranca23() {
+    const audio = document.getElementById('audiotb23');
+    audio.play();
+  }
+  
+  
+  function teclabranca24() {
+    const audio = document.getElementById('audiotb24');
+    audio.play();
+  }
+  
+  
+  function teclabranca25() {
+    const audio = document.getElementById('audiotb25');
+    audio.play();
+  }
+  
+  
+  function teclabranca26() {
+    const audio = document.getElementById('audiotb26');
+    audio.play();
+  }
+  
+  
+  function teclabranca27() {
+    const audio = document.getElementById('audiotb27');
+    audio.play();
+  }
+  
+  
+  function teclabranca28() {
+    const audio = document.getElementById('audiotb28');
+    audio.play();
+  }
+
+  function teclapreta1() {
+    const audio = document.getElementById('audiotb28');
+    audio.play();
+  }
+
+  function teclapreta1() {
+    const audio = document.getElementById('audiotb1');
+    audio.play();
+  }
+  function teclapreta2() {
+    const audio = document.getElementById('audiotb2');
+    audio.play();
+  }
+  function teclapreta3() {
+    const audio = document.getElementById('audiotb3');
+    audio.play();
+  }
+  function teclapreta4() {
+    const audio = document.getElementById('audiotb4');
+    audio.play();
+  }
+  function teclapreta5() {
+    const audio = document.getElementById('audiotb5');
+    audio.play();
+  }
+  function teclapreta6() {
+    const audio = document.getElementById('audiotb6');
+    audio.play();
+  }
+  function teclapreta7() {
+    const audio = document.getElementById('audiotb7');
+    audio.play();
+  }
+  function teclapreta8() {
+    const audio = document.getElementById('audiotb8');
+    audio.play();
+  }
+  function teclapreta9() {
+    const audio = document.getElementById('audiotb9');
+    audio.play();
+  }
+  function teclapreta10() {
+    const audio = document.getElementById('audiotb10');
+    audio.play();
+  }
+  function teclapreta11() {
+    const audio = document.getElementById('audiotb11');
+    audio.play();
+  }
+  function teclapreta12() {
+    const audio = document.getElementById('audiotb12');
+    audio.play();
+  }
+  function teclapreta13() {
+    const audio = document.getElementById('audiotb13');
+    audio.play();
+  }
+  function teclapreta14() {
+    const audio = document.getElementById('audiotb14');
+    audio.play();
+  }
+  function teclapreta15() {
+    const audio = document.getElementById('audiotb15');
+    audio.play();
+  }
+  function teclapreta16() {
+    const audio = document.getElementById('audiotb16');
+    audio.play();
+  }
+  function teclapreta17() {
+    const audio = document.getElementById('audiotb17');
+    audio.play();
+  }
+  function teclapreta18() {
+    const audio = document.getElementById('audiotb18');
+    audio.play();
+  }
+  function teclapreta19() {
+    const audio = document.getElementById('audiotb19');
+    audio.play();
+  }
+  function teclapreta20() {
+    const audio = document.getElementById('audiotb20');
+    audio.play();
+  }
+  
+  
+  
+  
+  
